f8d9df27c2536421dd275d5f31a9b8ba17c7a2ff
Арина <rina@MacBook-Air-Arina.local>
2020-05-27 04:18:46 +0300

    move logic to reducer

--- src/store/reducers/lampReducer.js
+++ src/store/reducers/lampReducer.js
@@ -1,6 +1,7 @@
 import {
-    TOGGLE_SWITCHER, TOGGLE_MODE, CHANGE_BRIGHTNESS
+    TOGGLE_SWITCHER, TOGGLE_MODE, INCREASE_BRIGHTNESS, DECREASE_BRIGHTNESS
 } from "../constants/actionsTypes";
+import {BRIGHTNESS_STATES, LAMP_STATES} from "../constants/states";
 
 const initialState = {
     mode: 0,
@@ -8,22 +9,31 @@ const initialState = {
     enabled: true
 };
 
-export default function lampReducer(state = initialState, {type, payload}) {
+export default function lampReducer(state = initialState, {type}) {
     switch (type) {
         case TOGGLE_MODE:
             return {
                 ...state,
-                mode: payload.mode,
+                mode: LAMP_STATES[state.mode + 1] || LAMP_STATES[0],
             };
-        case CHANGE_BRIGHTNESS:
+        case INCREASE_BRIGHTNESS:
             return {
                 ...state,
-                brightness: payload.brightness
+                brightness: Math.min(BRIGHTNESS_STATES.MAX_BRIGHTNESS,
+                    Number((state.brightness + BRIGHTNESS_STATES.STEP).toFixed(1)))
+            };
+        case DECREASE_BRIGHTNESS:
+            return {
+                ...state,
+                brightness: Math.max(BRIGHTNESS_STATES.MIN_BRIGHTNESS,
+                    Number((state.brightness - BRIGHTNESS_STATES.STEP).toFixed(1)))
             };
         case TOGGLE_SWITCHER:
             return {
                 ...state,
-                enabled: payload.enabled
+                enabled: !state.enabled,
+                mode: state.enabled ? 2 : 0,
+                brightness: BRIGHTNESS_STATES.INITIAL_BRIGHTNESS
             }
         default:
             return state;
